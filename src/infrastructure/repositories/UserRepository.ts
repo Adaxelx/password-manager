7615288a1340c626504e7505e8e8b532bffb633c
Adrian Mostowski <adaxelx@o2.pl>
2022-01-16 00:50:58 +0100

    authentication: add salt and pepper to login/register

--- src/infrastructure/repositories/UserRepository.ts
+++ src/infrastructure/repositories/UserRepository.ts
@@ -1,12 +1,14 @@
 import {User, UserCredentials} from 'core/User'
 import {PrismaClient} from '@prisma/client'
+import {compareData, hashData} from '../../utils/helpers'
 
 const prisma = new PrismaClient()
 
 export const loginUser = async ({email, password}: UserCredentials) => {
   try {
     const user = await prisma.user.findUnique({where: {email}})
-    if (user && user.password === password) {
+
+    if (user && compareData(password, user.password)) {
       return true
     }
     return false
@@ -17,13 +19,16 @@ export const loginUser = async ({email, password}: UserCredentials) => {
 
 export const registerUser = async (userData: User) => {
   const {email, password, name} = userData
+  const passwordHashed = hashData(password)
   try {
     const existingUser = await prisma.user.findUnique({where: {email}})
     if (existingUser) {
       return 'exist'
     }
-    const user = await prisma.user.create({data: userData})
-    return user
+    const user = await prisma.user.create({
+      data: {...userData, password: passwordHashed},
+    })
+    return {id: user.id, email: user.email, name: user.name}
   } catch (err) {
     return false
   }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,17 @@
+import bcrypt from 'bcrypt'
+
+const saltRounds = process.env.SALT_ROUNDS
+const pepper = process.env.PEPPER
+
 export function checkIfValidData<T>(data: any): data is T {
   return typeof data === 'object' && !Array.isArray(data) && data
 }
+
+export const hashData = (data: string | Buffer) => {
+  const salt = bcrypt.genSaltSync(saltRounds ? parseInt(saltRounds) : 10)
+  const hashed = bcrypt.hashSync(data, salt + pepper)
+
+  return hashed
+}
+export const compareData = (data: string, encrypted: string) =>
+  bcrypt.compareSync(data, encrypted)
